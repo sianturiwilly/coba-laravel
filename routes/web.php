9d3410485b8103078a2f357605150fb9c3275cef
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-16 21:14:00 +0700

    Commit kedua

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Willi Sianturi",
+        "email" => "willymusermufc@hotmail.com",
+        "image" => "willi.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
