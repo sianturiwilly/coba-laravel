d6bebcdd621952c9ebeb9994375fe57234255852
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-17 15:46:02 +0700

    Commit ke-6

--- database/wpu_blog.sql
+++ database/wpu_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2022 at 06:16 AM
+-- Generation Time: Oct 17, 2022 at 10:43 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -57,7 +57,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_10_17_072417_create_posts_table', 1);
 
 -- --------------------------------------------------------
 
@@ -92,6 +93,32 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `excerpt` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `published_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `slug`, `excerpt`, `body`, `published_at`, `created_at`, `updated_at`) VALUES
+(1, 'Judul Pertama', 'judul-pertama', 'Kutipan pertama', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in quae voluptate nihil veniam quia eveniet eaque quo, vitae quisquam eos at porro suscipit cumque commodi explicabo impedit sequi, illum qui!</p><p>Recusandae doloremque consectetur possimus, quaerat eos debitis vitae provident error alias, velit, quos numquam eius sequi iusto ducimus doloribus ipsa esse eligendi. Beatae doloribus vero animi adipisci in officia et ut consequatur distinctio nam aperiam consequuntur sit quasi quibusdam iste dolores atque natus, necessitatibus deserunt delectus non fugiat id?</p><p>Eius quod molestias id perspiciatis accusamus sed totam numquam rem magni? Natus sunt ipsam facilis. At quasi amet expedita sit provident placeat, inventore ipsa veritatis reprehenderit nihil blanditiis non tempore rerum asperiores quas, neque similique mollitia nam delectus maiores atque distinctio laboriosam hic eius! Nostrum minus beatae in, fugit doloribus saepe porro quos totam aut! Ad rem, quam sed maiores consectetur debitis sint voluptatibus mollitia laboriosam hic recusandae esse harum?</p>', NULL, '2022-10-17 01:36:50', '2022-10-17 01:36:50'),
+(2, 'Judul Kedua', 'judul-kedua', 'Kutipan kedua', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in quae voluptate nihil veniam quia eveniet eaque quo, vitae quisquam eos at porro suscipit cumque commodi explicabo impedit sequi, illum qui!</p><p>Recusandae doloremque consectetur possimus, quaerat eos debitis vitae provident error alias, velit, quos numquam eius sequi iusto ducimus doloribus ipsa esse eligendi. Beatae doloribus vero animi adipisci in officia et ut consequatur distinctio nam aperiam consequuntur sit quasi quibusdam iste dolores atque natus, necessitatibus deserunt delectus non fugiat id?</p><p>Eius quod molestias id perspiciatis accusamus sed totam numquam rem magni? Natus sunt ipsam facilis. At quasi amet expedita sit provident placeat, inventore ipsa veritatis reprehenderit nihil blanditiis non tempore rerum asperiores quas, neque similique mollitia nam delectus maiores atque distinctio laboriosam hic eius! Nostrum minus beatae in, fugit doloribus saepe porro quos totam aut! Ad rem, quam sed maiores consectetur debitis sint voluptatibus mollitia laboriosam hic recusandae esse harum?</p>', NULL, '2022-10-17 01:37:24', '2022-10-17 01:37:24'),
+(3, 'Judul Ketiga', 'judul-ketiga', 'Kutipan ketiga', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad in quae voluptate nihil veniam quia eveniet eaque quo, vitae quisquam eos at porro suscipit cumque commodi explicabo impedit sequi, illum qui!</p><p>Recusandae doloremque consectetur possimus, quaerat eos debitis vitae provident error alias, velit, quos numquam eius sequi iusto ducimus doloribus ipsa esse eligendi. Beatae doloribus vero animi adipisci in officia et ut consequatur distinctio nam aperiam consequuntur sit quasi quibusdam iste dolores atque natus, necessitatibus deserunt delectus non fugiat id?</p><p>Eius quod molestias id perspiciatis accusamus sed totam numquam rem magni? Natus sunt ipsam facilis. At quasi amet expedita sit provident placeat, inventore ipsa veritatis reprehenderit nihil blanditiis non tempore rerum asperiores quas, neque similique mollitia nam delectus maiores atque distinctio laboriosam hic eius! Nostrum minus beatae in, fugit doloribus saepe porro quos totam aut! Ad rem, quam sed maiores consectetur debitis sint voluptatibus mollitia laboriosam hic recusandae esse harum?</p>', NULL, '2022-10-17 01:37:49', '2022-10-17 01:37:49');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -105,14 +132,6 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Willi Sianturi', 'prajudiwilliam10@gmail.com', '$2y$10$SJ27LLvL3hmU2eYrHIbdL.KWXfBuB.NgyiUqbxC6oY1ApBgFxI6H6', NULL, NULL, '2022-10-16 21:10:57', '2022-10-16 21:10:57'),
-(2, 'Willi Willy', 'willymusermufc@hotmail.com', '$2y$10$AqFq5gHuxFTvGiHX7cZyceuv4mZEn0p7X2PzIZv5w.lZRgfyfSXfC', NULL, NULL, '2022-10-16 21:14:19', '2022-10-16 21:14:19');
 
 --
 -- Indexes for dumped tables
@@ -146,6 +165,13 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `posts_slug_unique` (`slug`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -166,7 +192,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -175,10 +201,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
