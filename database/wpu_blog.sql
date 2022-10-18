d6aab7a2c5190db098c4e09263bd634bbe6dd658
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-18 15:23:35 +0700

    Commit ke-11

--- database/wpu_blog.sql
+++ database/wpu_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2022 at 07:34 AM
+-- Generation Time: Oct 18, 2022 at 10:21 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
