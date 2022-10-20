6a2d69fcf4aa3176f01cce115cf5172f8a596271
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-20 13:49:01 +0700

    Commit ke-17

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,23 +2,32 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-md-4">
-        <main class="form-signin">
-            <h1 class="h3 mb-3 fw-normalvtext-center">Please Login</h1>
-            <form>
-              <div class="form-floating">
-                <input type="email" name="email" class="form-control" id="email" placeholder="Email">
-                <label for="email">Email Address</label>
-              </div>
-              <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="password" placeholder="Password">
-                <label for="email">Password</label>
-              </div>
+  <div class="col-md-4">
+
+    @if(session()->has('success'))
+      <div class="alert alert-success alert-dismissible fade show"  role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    @endif
+
+    <main class="form-signin">
+      <h1 class="h3 mb-3 fw-normalvtext-center">Please Login</h1>
+      <form action="/login" method="post">
+        @csrf
+        <div class="form-floating">
+          <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" autofocus required>
+          <label for="email">Email Address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+          <label for="password">Password</label>
+        </div>
           
-              <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-            </form>
-            <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
-        </main>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+      </form>
+      <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
+    </main>
     </div>
 </div>
 @endsection
