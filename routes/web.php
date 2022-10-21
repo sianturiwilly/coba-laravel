0e5e51e9eb4a14e3ca96553ac3905f167b9102f6
sianturiwilly <sianturiwilly>
2022-10-21 19:08:49 +0700

    Commit ke-5

--- routes/web.php
+++ routes/web.php
@@ -47,14 +47,14 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
-// Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 // Route::get('/dashboard', function() {
 //     return view('dashboard.index');
 // })->middleware('auth');
