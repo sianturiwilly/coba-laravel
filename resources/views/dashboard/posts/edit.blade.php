c9c4b776d6ab2db5565c71262d1496dbe75350d9
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-24 23:23:16 +0700

    Commit ke-16

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="col-lg-8">
-    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="mb-5">
+    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="mb-5" enctype="multipart/form-data">
         @method('put')
         @csrf
         <div class="mb-3">
@@ -18,6 +18,7 @@
             </div>
           @enderror
         </div>
+
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
             <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug', $post->slug) }}">
@@ -27,6 +28,7 @@
                 </div>
             @enderror
         </div>
+
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
             <select class="form-select" name="category_id">
@@ -40,6 +42,23 @@
                 @endforeach
             </select>
         </div>
+
+        <div class="mb-3">
+            <label for="image" class="form-label">Post Image</label>
+            <input type="hidden" name="oldImage" value="{{ $post->image }}">
+            @if($post->image)
+                <img src="{{ asset('storage/' . $post->iamge) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+            @else 
+                <img class="img-preview img-fluid mb-3 col-sm-5">
+            @endif
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+            @error('image')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
         <div class="mb-3">
             <label for="body" class="form-label">Body</label>
             @error('body')
@@ -65,6 +84,20 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     })
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview =document.querySelector ('.img-preview');
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 
 @endsection
